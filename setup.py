46aabad61bb99ed3b0682df87754aeecff886459
sjjeong <sjjeong94@gmail.com>
2022-05-24 23:33:33 +0900

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='videocv',
+    packages=find_packages(),
+    version='0.1.0',
+    license='MIT',
+    description='Video & OpenCV',
+    author='sjjeong94',
+    author_email='sjjeong94@gmail.com',
+    url='https://github.com/sjjeong94/videocv',
+    install_requires=['opencv-python'],
+)
